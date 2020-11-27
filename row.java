d8c21c5220c4120bade905bbcadef1bc412c6218
OnelGundran <55194708+OnelGundran@users.noreply.github.com>
2020-11-27 14:30:07 +0800

    Create row.java

---
+++ row.java
@@ -0,0 +1,17 @@
+package com.example.sqlchallenge.model;
+
+public class Row<var> {
+    var name:String?=null;
+    var name:String?=null;
+    var name:String?=null;
+
+    constructor () ()
+
+    constructor (name:String) ()
+    {
+        this.name=name
+        this.name=name
+        this.name=name
+    }
+
+}
